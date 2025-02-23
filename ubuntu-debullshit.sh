d73c11a6c838cf251603702372d90c75a819f3f1
Pedro Jelev <pedroalves2801@gmail.com>
2025-02-23 13:06:52 -0300

    Add: setup_zsh

--- ubuntu-debullshit.sh
+++ ubuntu-debullshit.sh
@@ -1,51 +1,92 @@
 #!/usr/bin/env bash
 
 remove_snaps() {
-  while [ "$(snap list | wc -l)" -gt 0 ]; do
-    for snap in $(snap list | tail -n +2 | cut -d ' ' -f 1); do
-      snap remove --purge "$snap"
+    while [ "$(snap list | wc -l)" -gt 0 ]; do
+        for snap in $(snap list | tail -n +2 | cut -d ' ' -f 1); do
+            snap remove --purge "$snap"
+        done
     done
-  done
 
-  systemctl stop snapd
-  systemctl disable snapd
-  systemctl mask snapd
-  apt purge snapd -y
-  rm -rf /snap /var/lib/snapd
-  for userpath in /home/*; do
-    rm -rf $userpath/snap
-  done
-  cat <<-EOF | tee /etc/apt/preferences.d/nosnap.pref
-      Package: snapd
-      Pin: release a=*
-      Pin-Priority: -10
-      EOF 
+    systemctl stop snapd
+    systemctl disable snapd
+    systemctl mask snapd
+    apt purge snapd -y
+    rm -rf /snap /var/lib/snapd
+    for userpath in /home/*; do
+        rm -rf $userpath/snap
+    done
+    cat <<-EOF | tee /etc/apt/preferences.d/nosnap.pref
+	Package: snapd
+	Pin: release a=*
+	Pin-Priority: -10
+	EOF
 }
 
 update_system() {
-  apt update && apt upgrade -y
+    apt update && apt upgrade -y
 }
 
 cleanup() {
-  apt autoremove -y
+    apt autoremove -y
 }
 
-set_flathub() {
-  apt install flatpak -y
-  flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-  apt install gnome-software -y
+setup_flathub() {
+    apt install flatpak -y
+    flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+    apt install --install-suggests gnome-software -y
 }
 
-set_zsh() {
-  apt install zsh
-  chsh -s $(which zsh)
+ask_reboot() {
+    echo 'Reboot now? (y/n)'
+    while true; do
+        read choice
+        if [[ "$choice" == 'y' || "$choice" == 'Y' ]]; then
+            reboot
+            exit 0
+        fi
+        if [[ "$choice" == 'n' || "$choice" == 'N' ]]; then
+            break
+        fi
+    done
+}
+
+msg() {
+    tput setaf 2
+    echo "[*] $1"
+    tput sgr0
+}
+
+error_msg() {
+    tput setaf 1
+    echo "[!] $1"
+    tput sgr0
+}
+
+check_root_user() {
+    if [ "$(id -u)" != 0 ]; then
+        echo 'Please run the script as root!'
+        echo 'We need to do administrative tasks'
+        exit
+    fi
+}
+
+setup_zsh() {
+    apt install zsh -ychsh
+    chsh -s $(which zsh)
 }
 
 main() {
-  update_system
-  remove_snaps
-  set_flathub
-  set_zsh
+    check_root_user
+    msg 'Atualizando lista de repositórios'
+    update_system
+    msg 'Removendo snaps'
+    remove_snaps
+    msg 'Instalando e configurando flathub'
+    setup_flathub
+    setup_zsh
+    cleanup
+    ask_reboot
 }
 
-main
+
+(return 2> /dev/null) || main
